700078763ecd62b7adbd4e7539cd422de63131fe
Golfetto Thomas <golfetto.thomas@gmail.com>
2022-10-19 15:31:54 +0200

    MVC architecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dima2022/main.dart';
+import 'package:dima2022/model_view/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
